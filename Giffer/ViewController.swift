665a8542e73728253abde51d815e4ccecb23c9c0
glenn-po <glennfor72@gmail.com>
2023-08-14 01:05:19 -0400

    Initial Commit

---
+++ Giffer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Giffer
+//
+//  Created by Glen Nfor on 8/14/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
