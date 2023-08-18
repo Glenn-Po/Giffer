f3511fc40794c207170ee1dc9eb2122ef35e9926
glenn-po <glennfor72@gmail.com>
2023-08-18 01:38:37 -0400

    First commit

--- Giffer/ViewController.swift
+++ Giffer/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var changeBackgroundColor: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+         // Do any additional setup after loading the view.
     }
+    func changeColor() -> UIColor{
 
+            let red = CGFloat.random(in: 0...1)
+            let green = CGFloat.random(in: 0...1)
+            let blue = CGFloat.random(in: 0...1)
 
+            return UIColor(red: red, green: green, blue: blue, alpha: 0.5)
+        }
+
+    
+    
+    @IBAction func changeBackgroundColor(_ sender: UIButton) {
+        let randomColor = changeColor()
+            view.backgroundColor = randomColor
+      
+    }
 }
 
---
+++ README.md
@@ -0,0 +1,38 @@
+# Giffer
+
+A small application that displays some basic info and changes the app background on button click
+
+
+## Exercise
+
+### Favorite Apps and their Features
+
+#### Whatsapp
+- Status feature
+- Intuitive icons
+- Pinning chats
+- Disappearing messages
+
+#### YouTube
+- Minimized player
+- Infinite scroll
+- Relevant recommendations
+- Live Chat feature
+
+
+#### 
+#### Discord
+- Transition animations between screens
+- Minimalist side bar
+- Tagging feature
+
+#### LinkedIn
+- Accessible and relevant bottom bar
+- Multiple email sign in
+- Share profile updates with connections
+
+
+### App Idea
+
+Streakz: A small app that keeps a streak for activities over intervals of time(day) 
+
